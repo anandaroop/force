6a67da73f8e126cd7f68332d1101fa7b9319cb59
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-09-07 13:47:52 -0400

    Add Waypoints and use them to update highlight state

--- desktop/apps/categories3/components/GeneFamily.js
+++ desktop/apps/categories3/components/GeneFamily.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import Waypoint from 'react-waypoint'
 
 import Gene from './Gene'
 import { alphabetizeGenes } from '../utils.js'
@@ -10,7 +11,8 @@ const propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   genes: PropTypes.array.isRequired,
-  featuredGenes: PropTypes.object
+  featuredGenes: PropTypes.object,
+  onHighlightFamily: PropTypes.func.isRequired
 }
 
 const GeneFamilyName = styled.h2`
@@ -28,19 +30,39 @@ const GeneList = styled.ul`
   }
 `
 
-const GeneFamily = ({ id, name, genes, featuredGenes }) => {
-  const sortedGenes = alphabetizeGenes(genes)
-  return (
-    <div id={id}>
-      <GeneFamilyName>
-        {name}
-      </GeneFamilyName>
-      <FeaturedGenes featuredGenes={featuredGenes} />
-      <GeneList>
-        {sortedGenes.map(gene => <Gene key={gene.id} {...gene} />)}
-      </GeneList>
-    </div>
-  )
+class GeneFamily extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleWaypointPositionChange = this.handleWaypointPositionChange.bind(this)
+  }
+
+  handleWaypointPositionChange ({ currentPosition, previousPosition }) {
+    const { id, onHighlightFamily } = this.props
+    const enteringAtTop =
+      previousPosition === 'above' && currentPosition === 'inside'
+    const exitingAtTop =
+      previousPosition === 'inside' && currentPosition === 'above'
+    if (enteringAtTop || exitingAtTop) onHighlightFamily(id)
+  }
+
+  render () {
+    const { id, name, genes, featuredGenes } = this.props
+    const sortedGenes = alphabetizeGenes(genes)
+    return (
+      <div id={id}>
+        <Waypoint
+          key={id}
+          topOffset={95}
+          onPositionChange={this.handleWaypointPositionChange}
+        />
+        <GeneFamilyName>{name}</GeneFamilyName>
+        <FeaturedGenes featuredGenes={featuredGenes} />
+        <GeneList>
+          {sortedGenes.map(gene => <Gene key={gene.id} {...gene} />)}
+        </GeneList>
+      </div>
+    )
+  }
 }
 
 GeneFamily.propTypes = propTypes
